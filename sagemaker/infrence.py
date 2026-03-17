bce8d904c2674001f9abe0565838614b69e7cfbd
SUPRIYA KAMTHEKAR <supriya.kamthekar23@gmail.com>
2026-03-17 17:34:54 +0530

    adding sagemaker infrence.py file

--- sagemaker/infrence.py
+++ sagemaker/infrence.py
@@ -4,21 +4,21 @@ import os
 import pandas as pd
 
 def model_fn(model_dir):
-    model_path = os.path.join(model_dir,"model.pkl")
+    model_path = os.path.join(model_dir, "model.pkl")
     model = joblib.load(model_path)
     return model
 
 def input_fn(request_body, request_content_type):
     if request_content_type == "application/json":
-        data = json.load(request_body)
+        data = json.loads(request_body)   # ✅ FIXED
         return pd.DataFrame([data])
     
-    raise ValueError(f"Unsupported content type:{request_content_type}")
+    raise ValueError(f"Unsupported content type: {request_content_type}")
 
-def predict_fn(input_data,model):
+def predict_fn(input_data, model):
     prediction = model.predict(input_data)
-    return prediction[0]
+    return prediction   # ✅ return full array
 
-def output_fn(prediction,response_content_type):
-    result = "Yes" if prediction == 1 else "No"
-    return json.dumps({"churn_prediction":result})
+def output_fn(prediction, response_content_type):
+    result = ["Yes" if p == 1 else "No" for p in prediction]
+    return json.dumps({"churn_prediction": result})
